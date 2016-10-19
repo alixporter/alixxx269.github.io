ab7479c68d625e9cd55d604e87ec40f038c18f32
alixxx269 <alixporter26@gmail.com>
2016-10-19 22:04:01 +1100

    Updating Webpages

--- js/avm.js
+++ js/avm.js
@@ -7,20 +7,43 @@ var images = [
     "images/DSC6386pics.jpg",
     "images/DSC6377pics.jpg"];
 
-		function randImg() {
-    		var size = images.length
-    		var x = Math.floor(size * Math.random())
-    		$('#randompic').css("background-image","url("+images[x]+")");
+var backgrounds = [
+ 	"images/DSC6368pics.jpg",
+    "images/DSC6381pics.jpg",
+    "images/DSC6386pics.jpg",
+    "images/DSC6377pics.jpg"];
+
+var current=0;
+
+
+function randImg() {
+    var size = images.length
+    var x = Math.floor(size * Math.random())
+    $('#randompic').css("background-image","url("+images[x]+")");
 }
 
 randImg();
+
+
+function nextBackground() {
+	$(".biggallery").each(function (idx, elm) {
+		$(elm).css(
+        'background-image',
+    	"url('" + backgrounds[Math.floor(Math.random() * backgrounds.length)] + "')"
+    	);
+	});
+
+    setTimeout(nextBackground, 1000);
+}
+
+nextBackground();
+
 
 $('.state').hover(function() {
     $('.state-full', this).slideToggle(100, 'linear').display(100, 'linear');
 });
 
 
-// want to add in JS on the second page that allows the three big 
-// images to change all the time to different wreaths
+$('.feather').featherlight();
 
 });
